aa19a938afffd4c627be50cc1f79c8b285cbdc9e
ch <ch@mail.startek-eng.com>
2019-05-06 11:28:01 +0800

    Working with custom-type Arrays
    sec. 2, video 17

--- src/index.js
+++ src/index.js
@@ -2,12 +2,45 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [{
+    id: '1',
+    name: 'Hsu',
+    email: 'hsu@test.com',
+    age: 28
+}, {
+    id: '2',
+    name: 'Sara',
+    email: 'sara@test.com'
+}, {
+    id: '3',
+    name: 'John',
+    email: 'john@test.com',
+    age: 35
+}]
+
+const posts = [{
+    id: '1',
+    title: 'What a day',
+    body: 'It is a good day.',
+    published: true
+}, {
+    id: '2',
+    title: 'GraphQL',
+    body: 'Still work on it...',
+    published: false
+}, {
+    id: '3',
+    title: 'Golang',
+    body: 'I need to spend more time on it!',
+    published: false
+}]
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String, position: String): String!
-        add(numbers: [Float!]!): Float!
-        grades: [Int!]!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
@@ -30,23 +63,25 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
     Query: {
-        greeting(parent, args, ctx, info) {
-            if (args.name && args.position) {
-                return `Hello, ${args.name}! You are my favoraite ${args.position}.`
-            }
-            return 'Hello!'
-        },
-        add(parent, args, ctx, info) {
-            if (args.numbers.length === 0) {
-                return 0
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
             }
 
-            return args.numbers.reduce((accumulator, currentValue) => {
-                return accumulator + currentValue
+            return users.filter((user) => {
+                // case-insensitive search
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
             })
         },
-        grades(parent, args, ctx, info) {
-            return [85, 93, 60]
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            const query = args.query.toLowerCase()
+            return posts.filter((post) => {
+                return post.title.toLowerCase().includes(query) || post.body.toLowerCase().includes(query)
+            })
         },
         me() {
             return {
